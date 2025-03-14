2a7160a66f351912bcb7484e0e305992594e39bf
panchal777 <panchalravina777@gmail.com>
2025-03-14 18:39:34 +0530

    - added bloc - exception handling - clean code architecture

---
+++ lib/core/network/error/exceptions.dart
@@ -0,0 +1,29 @@
+class ServerException implements Exception {
+  final String message;
+  final int? statusCode;
+
+  ServerException(this.message, this.statusCode);
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) {
+      return true;
+    }
+
+    if (other.runtimeType != runtimeType) {
+      return false;
+    }
+    if (other is ServerException) {
+      return other.message == message && other.statusCode == statusCode;
+    }
+
+    return false;
+  }
+}
+
+class CancelTokenException implements Exception {
+  final String message;
+  final int? statusCode;
+
+  CancelTokenException(this.message, this.statusCode);
+}
